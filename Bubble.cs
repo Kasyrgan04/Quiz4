595981545084c64fe00670a0c0253df887100a38
Kasyrgan04 <dyguerrero@estudiantec.cr>
2024-10-22 08:37:38 -0600

    Inicial

---
+++ Bubble.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz4
+{
+    internal class Bubble
+    {
+        public static void bubbleSort(int[] list)
+        {
+            int n = list.Length;
+            int i, j, temp;
+            bool swapped;
+            for (i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+                for (j = 0; j < n - i - 1; j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                if (swapped == false)
+                    break;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Quiz4;
+using System.Diagnostics;
+Stopwatch Tiempo = new Stopwatch();
+int[] list = new int[100000];
+Random rnd = new Random();
+for (int i = 0; i < list.Length; i++)
+{
+    list[i] = rnd.Next(0, 10000000);
+}
+Tiempo.Start();
+//Quick.QuickSort(list, 0, list.Length - 1);
+Bubble.bubbleSort(list);
+Tiempo.Stop();
+Console.WriteLine("Quick Sort: " + Tiempo.Elapsed.TotalMilliseconds + " ms");
